ff42d0c8520a071b2b7307fbc433a431eec8ef34
Thanapat1502 <psy150201027@gmail.com>
2024-10-03 12:17:14 +0700

    feat: answer ex1-ex6

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,21 @@
 // Start coding here
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(5, 10));
+console.log(subtract(5, 10));
+console.log(multiply(5, 10));
+console.log(divide(10, 5));
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+let add = function (a, b) {
+  return a + b;
+};
+
+let subtract = function (a, b) {
+  return a - b;
+};
+
+let multiply = function (a, b) {
+  return a * b;
+};
+
+let divide = function (a, b) {
+  return a / b;
+}
+
+console.log(add(5, 10));
+console.log(subtract(5, 10));
+console.log(multiply(5, 10));
+console.log(divide(10, 5));
